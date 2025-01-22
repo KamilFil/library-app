16b524ab79f9199b12f07d4b554fd8073f5bdfce
KamilFil <kamilfprog@gmail.com>
2025-01-23 00:42:07 +0100

    config/install-tanstack-router

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,8 @@
+import { createRootRoute } from '@tanstack/react-router';
 import { Box, styled, Typography } from '@mui/material';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
-export const App = () => {
+export const HomePage = () => {
   const StyledBox = styled(Box)({
     display: 'flex',
     justifyContent: 'center',
@@ -14,3 +15,7 @@ export const App = () => {
     </StyledBox>
   );
 };
+
+export const Route = createRootRoute({
+  component: HomePage,
+});
