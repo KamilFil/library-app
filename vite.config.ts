fdd1ba1e923b3fb6e885f7ed9bca418f5712317d
KamilFil <kamilfprog@gmail.com>
2025-01-23 01:22:37 +0100

    config/install-vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
@@ -6,6 +7,11 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   plugins: [TanStackRouterVite({}), react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest.setup.ts',
+  },
   server: {
     port: 3000,
   },
