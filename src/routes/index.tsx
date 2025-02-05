d3befa5af390e50422b2ce34bb69df9f19c3d2e8
PiotrB95 <piotr.bubnowski95@gmail.com>
2025-02-05 16:32:23 +0100

    Delete unnecessary main page components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,7 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
-  component: RouteComponent,
   beforeLoad: () => {
     throw redirect({ to: '/books', search: { page: 1, size: 5 } });
   },
 });
-
-function RouteComponent() {
-  return <p>Main page</p>;
-}
--- src/routes/user/index.tsx
+++ src/routes/user/index.tsx
@@ -1,11 +1,6 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
-const RouteComponent = () => {
-  return <p>Main user page</p>;
-};
-
 export const Route = createFileRoute('/user/')({
-  component: RouteComponent,
   beforeLoad: () => {
     throw redirect({ to: '/user/my_stats' });
   },
