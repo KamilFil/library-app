f920b01ce9fb840a85d82d5f06ca0e0160da72c6
PiotrB95 <piotr.bubnowski95@gmail.com>
2025-02-05 17:48:33 +0100

    Added test script to lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,7 @@ export default {
   '*.{ts,tsx}': [
     'npm run prettify',
     () => 'npm run typecheck',
-    'npm run lint --fix --no-cache'
+    'npm run lint --fix --no-cache',
+    'npm run test'
   ],
 }
